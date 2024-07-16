cbbcb9f6a76462f30e81b11c3470de50025d3d01
Eddy <137077017+eddy531@users.noreply.github.com>
2024-07-16 08:36:51 -0500

    cambioUbu1

--- knexfile.js
+++ knexfile.js
@@ -9,8 +9,8 @@ module.exports = {
     client: 'mysql2',
     connection: {
       host: '127.0.0.1',
-      user: 'root',
-      password: '',
+      user: 'api_user1',
+      password: '1234',
       database: 'cloude'
     },
     migrations: {
